930c88b32555401ec0878d8beb8e078694abdca0
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-10-09 10:43:13 +0200

    added tag distribution

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -12,29 +12,31 @@ if __name__ == '__main__':
         f.close()
 
     with open('pos.csv', 'w', newline='') as f1:
+
             w1 = csv.writer(f1)
             w1.writerow(["title", "author", "author_rep_score", "tags" ])
             tag_stats = {}
             count=0
             lines = 0
             for item in final:
+                for tag in item["tags"]:
+
+                    if tag not in tag_stats.keys():
+                        tag_stats[tag] = 0
+                    else:
+                        tag_stats[tag] += 1
                 if item["tags"] == []:
                     count += 1
 
-                if item["score"]<=0:
+                if item["score"]>0:
                     w1.writerow([item["title"], item["user"]["name"],  item["user"]["reputation"], item["tags"] ])
                     lines+=1
-                    for tag in item["tags"]:
 
-                        if tag not in tag_stats.keys():
-                            tag_stats[tag] = 0
-                        else:
-                            tag_stats[tag] += 1
 
             print(lines)
             f1.close()
             sorted_x = sorted(tag_stats.items(), key=operator.itemgetter(1), reverse=True)
-            sorted_x=[ i for i in sorted_x if i[1]>100]
+            sorted_x=[ i for i in sorted_x if i[1]>0]
             print(len(sorted_x))
             print(lines)
             pprint(sorted_x)
